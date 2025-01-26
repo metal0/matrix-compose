b9b7e2360a48f2ef5fe0c008bc2a891fa1be1f38
Tiago R <metal@i0.tf>
2025-01-26 10:17:00 +0000

    round 2
    Signed-off-by: GitHub <noreply@github.com>

--- init.sh
+++ init.sh
@@ -27,7 +27,7 @@ ESCAPED_REGISTRATION_SECRET=$(printf '%s\n' "$REGISTRATION_SECRET" | sed -e 's/[
 ESCAPED_FORM_SECRET=$(printf '%s\n' "$FORM_SECRET" | sed -e 's/[]\/$*.^[]/\\&/g');
 ESCAPED_MACAROON_SECRET=$(printf '%s\n' "$MACAROON_SECRET" | sed -e 's/[]\/$*.^[]/\\&/g');
 
-find ./ -type f -exec sed -i -e "s|example.org|${TS_TAILNET}|g" {} \;
+find ./ -type f -exec sed -i -e "s|example.org|matrix.${TS_TAILNET}|g" {} \;
 find ./ -type f -exec sed -i -e "s|_PASSWORD_SALT_|${SALT}|g" {} \;
 find ./ -type f -exec sed -i -e "s|_ETURNAL_SECRET_|${ETURNAL_SECRET}|g" {} \;
 find ./ -type f -exec sed -i -e "s|_PG_PASSWORD_|${PG_PASSWORD}|g" {} \;
