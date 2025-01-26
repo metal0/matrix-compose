9271ce9c858a9b8a0c6ee0c7296f5a8e523d6ac6
Tiago R <metal@i0.tf>
2025-01-26 10:25:43 +0000

    -pantalaimon
    Signed-off-by: GitHub <noreply@github.com>

--- init.sh
+++ init.sh
@@ -7,8 +7,6 @@ export $(cat .env | xargs)
 sudo apt-get update && apt-get install -y openssl dig git
 openssl req -x509 -newkey rsa:4096 -keyout ./cert.key -out ./cert.pem -days 3650 -subj "/CN=${TS_TAILNET}" -nodes
 
-git clone https://github.com/matrix-org/pantalaimon.git pantalaimon
-
 docker compose pull
 
 docker compose build
