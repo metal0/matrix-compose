45183cae961ee927d52681d9d9d031c1e36f35da
Tiago R <metal@i0.tf>
2025-01-26 12:18:37 +0000

    oops
    Signed-off-by: GitHub <noreply@github.com>

--- init.sh
+++ init.sh
@@ -54,29 +54,29 @@ chmod -R 0770 ./data
 
 # Create registration files for mautrix bridges
 echo "Setup Bluesky"
-docker compose run mautrix-bluesky --remove-orphans
+docker compose run --remove-orphans mautrix-bluesky
 echo "Setup Discord"
-docker compose run mautrix-discord --remove-orphans
+docker compose run --remove-orphans mautrix-discord
 echo "Setup Meta"
-docker compose run mautrix-meta --remove-orphans
+docker compose run --remove-orphans mautrix-meta
 echo "Setup Google Messages"
-docker compose run mautrix-gmessages --remove-orphans
+docker compose run --remove-orphans mautrix-gmessages
 echo "Setup Google Chat"
-docker compose run mautrix-googlechat --remove-orphans
+docker compose run --remove-orphans mautrix-googlechat
 echo "Setup Instagram"
-docker compose run mautrix-instagram --remove-orphans
+docker compose run --remove-orphans mautrix-instagram
 echo "Setup LinkedIn"
-docker compose run beeper-linkedin --remove-orphans
+docker compose run --remove-orphans beeper-linkedin
 echo "Setup Signal"
-docker compose run mautrix-signal --remove-orphans
+docker compose run --remove-orphans mautrix-signal
 echo "Setup Slack"
-docker compose run mautrix-slack --remove-orphans
+docker compose run --remove-orphans mautrix-slack
 echo "Setup Telegram"
-docker compose run mautrix-telegram --remove-orphans
+docker compose run --remove-orphans mautrix-telegram
 echo "Setup Twitter"
-docker compose run mautrix-twitter --remove-orphans
+docker compose run --remove-orphans mautrix-twitter
 echo "Setup WhatsApp"
-docker compose run mautrix-whatsapp --remove-orphans
+docker compose run --remove-orphans mautrix-whatsapp
 echo "Setup Heisenbridge"
 docker run --rm --name heisenbridge-generate -v ./data/bridges/heisenbridge:/data hif1/heisenbridge:latest -c /data/registration.yaml -p 80 http://synapse:80 --generate
 sed -i 's/0.0.0.0:80/heisenbridge:80/g' ./data/bridges/heisenbridge/registration.yaml # xd
